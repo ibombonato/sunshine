3f77eacd3ee89ef7250c7da0146840f2733043a1
Icaro Bombonato <icaro@minhavida.com.br>
2014-07-26 16:54:41 -0300

    Creating adapter for forecast

--- app/src/main/java/br/com/icarobombonato/sunshine/MainActivity.java
+++ app/src/main/java/br/com/icarobombonato/sunshine/MainActivity.java
@@ -8,7 +8,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -57,7 +61,7 @@ public class MainActivity extends ActionBarActivity {
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-            String forecastEntries[] = {
+            String forecastArray[] = {
                     "Today - Sunny - 88/63",
                     "Tomorrow - Foggy - 70/46",
                     "Weds - Cloudy - 72/63",
@@ -65,6 +69,13 @@ public class MainActivity extends ActionBarActivity {
                     "Fri - Foggy - 70/46",
                     "Sat - Sunny - 76/68"
             };
+
+            List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
+
+            ArrayAdapter stringArrayAdapter = new ArrayAdapter<String>(getActivity(),
+                    R.layout.list_item_forecast,
+                    R.id.list_item_forecast_textView
+                    ,weekForecast);
 
             return rootView;
         }
